1f40d32dc2c5be4c0fb51f28b0991ff87b415787
sistemanpdvs <68880550+sistemanpdvs@users.noreply.github.com>
2021-03-31 17:28:59 -0300

    Add files via upload

--- npdvs.sh
+++ npdvs.sh
@@ -7,7 +7,6 @@
 # https://github.com/nilsonlinux/npdvs
 # -------------------------------------------------------
 #Variáveis
-UPDATE="wget -O /tmp/gmcorepdv.deb http://192.168.3.122/gmcorepdv.deb && dpkg -i /tmp/gmcorepdv.deb && reboot"
 pdvs_ips="101 102 103 104 105 106 107 108 109 110 111 112 113 114 115 116 117 118 119 120 121 122 123 124 125 126 127 128 129 130" #FINAL dos IPS DOS PDVS...
 GW="100"
 version="4.2"
